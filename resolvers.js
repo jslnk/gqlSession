988f2654dfac03daddde98aee34ce5ec0abc40b9
Jeongseon Lee <jlee@investorcom.com>
2020-11-19 21:52:00 -0500

    module the query and session

--- resolvers.js
+++ resolvers.js
@@ -1,30 +1,34 @@
 const _= require('lodash');
+const Query = require('./resolvers/query');
+const Session = require('./resolvers/session');
 
 module.exports = {
-    Query:{        
-        sessions:(parent, args, {dataSources}, info)=>{
-            return dataSources.sessionAPI.getSessions(args);
-        },
-        sessionById:(parent, {id}, {dataSources}, info) =>{
-            return dataSources.sessionAPI.getSessionByID(id);
-        }, 
-        speakers:(parent, args, {dataSources}, info) =>{
-            return dataSources.speakerAPI.getSpeakers();
-        },
-        speakerById:(parent, {id}, {dataSources}, info) =>{
-            return dataSources.speakerAPI.getSpeakerById(id);
-        }
-    },
+    Query,
+    Session
+    // Query:{        
+    //     sessions:(parent, args, {dataSources}, info)=>{
+    //         return dataSources.sessionAPI.getSessions(args);
+    //     },
+    //     sessionById:(parent, {id}, {dataSources}, info) =>{
+    //         return dataSources.sessionAPI.getSessionByID(id);
+    //     }, 
+    //     speakers:(parent, args, {dataSources}, info) =>{
+    //         return dataSources.speakerAPI.getSpeakers();
+    //     },
+    //     speakerById:(parent, {id}, {dataSources}, info) =>{
+    //         return dataSources.speakerAPI.getSpeakerById(id);
+    //     }
+    // },
 
-    Session:{
-        async speakers(session, args, {dataSources}){
-            const speakers = await dataSources.speakerAPI.getSpeakers();
-            const returns = speakers.filter((speaker)=>{
-            return _.filter(session.speakers, {id:speaker.id}).legnth > 0;
-            });
+    // Session:{
+    //     async speakers(session, args, {dataSources}){
+    //         const speakers = await dataSources.speakerAPI.getSpeakers();
+    //         const returns = speakers.filter((speaker)=>{
+    //         return _.filter(session.speakers, {id:speaker.id}).legnth>0;
+    //         });
 
-            return returns;
-        },
-    },
+    //         return returns;
+    //     }
+    // }
   };
   
